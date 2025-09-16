f2ad95b582a293f2dfaa499de86ec916de09c1b4
kafi <khankafi6.28@gmail.com>
2025-09-17 03:33:05 +0530

    tt

---
+++ updoowncase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char ch = 'h' ;
+    printf("the character is %c \n", ch);
+    printf("the char  %d \n", ch);
+    if (ch >=97 && ch<=122){
+        printf("lower case it is bro \n");
+
+    }
+    else 
+
+    {
+        printf("not the one u want \n");
+    }
+
+    return 0;
+}
