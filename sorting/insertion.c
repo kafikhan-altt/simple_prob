5a228b4296e63b17f487a85cb34f3120daf33e70
ABDUL KAFI KHAN <khankafi6.28@gmail.com>
2026-01-14 19:12:06 +0530

    added code line

---
+++ sorting/insertion.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+void insertionsort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+
+        while (prev >= 0 && arr[prev] > curr)
+        {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+       parray(arr, 5);
+}
+
+void parray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+}
+
+int main()
+{
+    int n = 5;
+
+    int arr[] = {4, 2, 5, 6, 3};
+
+    insertionsort(arr, 5);
+    return 0;
+
+}
