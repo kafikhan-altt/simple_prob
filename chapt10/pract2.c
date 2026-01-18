a4dfd12dc3d3286d65daf9462bb5d5772ad8bef9
ABDUL KAFI KHAN <khankafi6.28@gmail.com>
2026-01-18 16:18:38 +0530

    corrected

---
+++ chapt10/pract2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+FILE *ptr;
+ptr=fopen("sample.txt","a");
+int num=102;
+fprintf(ptr, "%d", num);
+
+fclose(ptr);
+
+return 0;
+
+
+}
---
+++ chapt10/pract3.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+    char ch;
+    FILE *ptr;
+
+    ptr = fopen("sample.txt", "r");
+    while (1)
+    {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+
+        if (ch == EOF)
+        {
+
+            break;
+        }
+    }
+    return 0;
+}
