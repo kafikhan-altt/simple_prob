36b32055baeee423610457cc84cda177e69a8cc7
ABDUL KAFI KHAN <khankafi6.28@gmail.com>
2026-01-18 15:12:22 +0530

    corrected

---
+++ chapt10/pract1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    
+
+FILE *ptr;
+ptr =fopen("sample.txt", "r");
+
+
+if(ptr == NULL){
+
+printf("the file does not exist sorry ! \n");
+
+}
+else{
+
+int num;
+fscanf(ptr, "%d", &num );
+printf("the vlue of num is %d \n", num);
+
+
+
+fscanf(ptr, "%d",&num );
+printf("the value of num is %d \n", num );
+
+fclose(ptr);
+
+}
+
+return 0;
+
+}
