75eb00eb8753572e67cdda6f2c7a58d4226451f0
ABDUL KAFI KHAN <khankafi6.28@gmail.com>
2026-01-19 06:04:27 +0530

    name changed

---
+++ func/q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void gn();
+
+void gm();
+
+void ge();
+
+
+void gm(){
+    printf("Good Morning\n");
+
+}
+
+void ge(){
+    printf("Good evening\n");
+
+}
+
+void gn(){
+    printf("Good night\n");
+
+}
+
+
+int main(){
+    
+ge();
+gn();
+gm();
+
+
+return 0;
+
+}
---
+++ func/q4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+
+
+
+int main(){
+    
+int a= 3;
+printf("sqaur of num is : %f", pow(a,2));
+    return 0;
+}
+
