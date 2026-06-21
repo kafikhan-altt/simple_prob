61385aa157b7830a0f2de063238d7e62c3abefbe
kafi <khankafi6.28@gmail.com>
2026-06-21 15:13:19 +0530

    Create prac.c

---
+++ linked_list/prac.c
@@ -0,0 +1,28 @@
+
+/*struct demo{
+    int data;
+    char ch;
+    struct demo *self;
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct node{
+    int data;
+    struct node *link;
+
+};
+
+
+int main() {
+    struct node *head=NULL;
+    head = (struct node *)malloc(sizeof(struct node));
+
+    head->data =45;
+    head->link =NULL;
+
+    printf("%d", head->data);
+    return 0;
+    
+}
