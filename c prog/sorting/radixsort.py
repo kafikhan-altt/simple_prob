fd11a796688ff045b72b077aa888b6e2e5212f76
kafi <khankafi6.28@gmail.com>
2026-09-10 23:05:48 +0530

    radix sort

---
+++ c prog/sorting/radixsort.py
@@ -0,0 +1,25 @@
+def counting_sort_for_radix(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+    for i in range(n):
+        index = (arr[i] // exp) % 10
+        count[index] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    for i in range(n - 1, -1, -1):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    if not arr:
+        return arr
+    max_num = max(arr)
+    exp = 1
+    while max_num // exp > 0:
+        counting_sort_for_radix(arr, exp)
+        exp *= 10
+    return arr
